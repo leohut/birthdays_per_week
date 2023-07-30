c7de3c471c705bc7a33fec684a8da5301117a3c3
leohut <132941625+leohut@users.noreply.github.com>
2023-07-30 18:31:23 +0200

    next commit

--- main.py
+++ main.py
@@ -1,32 +1,59 @@
 from datetime import datetime, timedelta
 
+
 def get_birthdays_per_week(users):
-       
-    birthdays_by_weekday = {i: [] for i in range(7)}  # Створюємо словник для зберігання іменинників на кожний день тижня.
-    for user in users:
-        birthday = user["birthday"].date() # Отримуємо день народження користувача з об'єкту datetime.
-        birthday_day_of_week = birthday.weekday() # Визначаємо день тижня, на який припадає день народження (понеділок - 0, неділя - 6).
-        
-        if birthday_day_of_week >= 5:  # Перевіряємо, чи є день тижня вихідним (субота або неділя). 5 і 6 - вихідні дні (субота, неділя).            
-            days_to_add = 2 if birthday_day_of_week == 5 else 1 # Знаходимо, скільки днів потрібно додати до дня народження, щоб перенести його.            
-            birthday = birthday + timedelta(days=days_to_add) # Знаходимо новий день для привітання.
-        
-        birthdays_by_weekday[birthday.weekday()].append(user["name"]) # Додаємо ім'я користувача до списку іменинників на відповідний день тижня.
     
-    for i in range(7):   # Виводимо іменинників
-        day_name = datetime(2000, 1, 3 + i).strftime('%A')  
-        if birthdays_by_weekday[i]:
-            print(f"{day_name}: {', '.join(birthdays_by_weekday[i])}")
+    current_date = datetime.now()  # Отримуємо поточну дату
+    
+    week_later_date = current_date + timedelta(days=7)  # Знаходимо дату через тиждень
+    
+    birthdays_per_week = {}  # Список для збереження користувачів, яких потрібно привітати по днях
+    for i in range(7):
+        birthdays_per_week[i] = []
+    
+    for user in users:  # Перетворюємо дати на тип datetime з часом 00:00:00
+        user["birthday"] = datetime(user["birthday"].year, user["birthday"].month, user["birthday"].day)
+    
+    for user in users: # Додаємо користувачів до відповідних списків днів тижня
+        birthday = user["birthday"]
+        name = user["name"]
+        birthday = birthday.replace(year=current_date.year)
+        
+        if current_date <= birthday < week_later_date: # Якщо день народження вихідний, то привітати його в понеділок
+            day_of_week = birthday.weekday()
+            if day_of_week >= 5:  # Вихідний день: субота (5) або неділя (6)
+                day_of_week = 0  # Переносимо на понеділок
+            
+            birthdays_per_week[day_of_week].append(name) # Використовуємо індекси, які відповідають дням тижня
+   
+    day_names = [    # Ім'я днів тижня для форматування результату
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+    
+    for day_index, names in birthdays_per_week.items():  # Виводимо результат у консоль
+        if names:
+            day_name = day_names[day_index]
+            names_str = ", ".join(names)
+            print(f"{day_name}: {names_str}")
             
             
-# тестовий список users:
+# Приклад тестових даних:
 users = [
-    {"name": "Alex", "birthday": datetime(2023, 7, 25)}, #вівторок
-    {"name": "Eva", "birthday": datetime(2022, 1, 2)}, #неділя
-    {"name": "Mikhael", "birthday": datetime(2022, 1, 1)}, #суббота
-    {"name": "John", "birthday": datetime(2023, 7, 27)},  # Четвер
-    {"name": "Mike", "birthday": datetime(2023, 7, 28)},  # П'ятниця
-    {"name": "Bill", "birthday": datetime(2023, 7, 30)},  # Неділя
-    {"name": "Enrique", "birthday": datetime(2023, 7, 26)},   #середа
+    {"name": "Bill", "birthday": datetime(1986, 8, 1)},
+    {"name": "Jill", "birthday": datetime(1970, 8, 2)},
+    {"name": "Kim", "birthday": datetime(1999, 8, 3)},
+    {"name": "Jan", "birthday": datetime(1978, 7, 28)},
+    {"name": "Bob", "birthday": datetime(1989, 8, 4)},
+    {"name": "Sam", "birthday": datetime(1990, 7, 31)},
+    {"name": "Mike", "birthday": datetime(1987, 7, 29)},
+    {"name": "Viki", "birthday": datetime(1983, 8, 5)},
 ]
-get_birthdays_per_week(users)
+
+if __name__ == "__main__":
+    get_birthdays_per_week(users)
